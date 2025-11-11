fcd97f1320ee6d88b70cc3525ebc11cd8be37722
Tachoviendo <ignacio.3249@gmail.com>
2025-11-11 11:35:42 -0300

    experimentoNacho

--- crmucu/src/Library/Models/utils/SistemaCRM.cs
+++ crmucu/src/Library/Models/utils/SistemaCRM.cs
@@ -1,1 +1,34 @@
 // la idea es inicializar todo desde esta clase
+//
+
+namespace CrmUcu.Models.Personas
+{
+    public class Etiqueta
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string? Color { get; set; }
+        public string? Descripcion { get; set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+        public Etiqueta() { }
+
+        public Etiqueta(string nombre, string? color = null, string? descripcion = null)
+        {
+            Nombre = nombre;
+            Color = color;
+            Descripcion = descripcion;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Etiqueta etiqueta && Id == etiqueta.Id;
+        }
+
+
+        public override string ToString()
+        {
+            return $"[{Nombre}] {Descripcion}";
+        }
+    }
+}
