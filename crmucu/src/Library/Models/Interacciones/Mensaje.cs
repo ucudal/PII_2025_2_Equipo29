162fb114886b6b6eca3367c385d2402e7eb1e81a
reberet <124381326+reberet@users.noreply.github.com>
2025-11-18 11:31:41 -0300

    Changes

--- crmucu/src/Library/Models/Interacciones/Mensaje.cs
+++ crmucu/src/Library/Models/Interacciones/Mensaje.cs
@@ -1,1 +1,22 @@
-//tenemos que codear esta claseeee
+using CrmUcu.Models.Enums;
+
+namespace CrmUcu.Models.Interacciones
+{
+    public class Mensaje : Interaccion 
+    {
+        public string Plataforma { get; set; }
+        public bool EsEntrante { get; set; }
+        public bool Leido { get; set; }
+
+        public Mensaje()
+        {
+            Tipo = TipoInteraccion.Mensaje;
+            Leido = false;
+        }
+
+        public void MarcarLeido()
+        {
+            Leido = true;
+        }
+    }
+}
